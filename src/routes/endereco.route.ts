d1f2fc10bbeec4370a03c247b6a5c37730d5eace
ItaloCST12 <ithallocostaap7@gmail.com>
2026-02-19 20:01:05 -0300

    Reorganiza backend para pasta BACK-END

--- src/routes/endereco.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { criarEndereco,} from "../controllers/endereco.controller";
-
-const router = Router();
-
-router.post("/", criarEndereco);
-
-export default router;
